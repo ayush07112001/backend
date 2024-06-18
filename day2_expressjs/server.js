aa37847051d9b85fdb20e7ff5f8080c0e6fa7f23
ayush07112001 <ayushpatel0711.ap@gmail.com>
2024-06-18 17:29:23 +0530

    day3

--- day2_expressjs/server.js
+++ day2_expressjs/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
+import bodyparser from 'express';
 const app=express();
+app.use(bodyparser.json())
 
 // CRUD
 // C=Create => POST
@@ -7,7 +9,7 @@ const app=express();
 //U=Update => PUT
 //D=Delete => DELETE
 
-const product=[
+let product=[
     {id:1,title:'iphone',price:75000},
     {id:2,title:'oneplus 8',price:35000},
     {id:3,title:'sony xperia',price:55000},
@@ -29,11 +31,43 @@ app.get('/movies',(req,res)=>{
     res.send(movies)
 })
 
-// get product by id
+// get product by id  [dynamic routing.....]
 app.get('/product/:id',(req,res)=>{
-    console.log("dynamic routing",req.params)
+    const id=req.params.id
+    const result=product.filter((data)=>data.id==id)
+    res.send(result);
+    // console.log("dynamic routing",id)
+})
+
+// add product
+app.post('/add',(req,res)=>{
+    //   console.log(req.body) 
+    product.push(req.body)
+    res.send("your product has been added"+req.body)
 })
 
 
+// delete product
+app.delete('/product/:id',(req,res)=>{
+    const id=req.params.id
+    product=product.filter((data)=>data.id!=id);
+    res.json({message:'your product has been deleted',product})
+
+})
+
+// update product by id;
+app.put("/product/:id",(req,res)=>{
+    const id=req.params.id;
+    let data=req.body
+    console.log(data)
+    for(let i=0;i<product.length;i++){
+        if(id==product[i].id){
+            product[i].title=data.title
+            product[i].price=data.price
+            break;
+        }
+    }
+    res.json({message:'your product has been updated'})
+})
 const port=1000;
 app.listen(port,()=>console.log(`server is running on port ${port}`));
